fab28d06b7407d64c7e2879b641de6a01d41b4e5
AmanMaverick <aman.rocks923@gmail.com>
2018-06-11 15:41:47 +0530

    Added First File

---
+++ First.java
@@ -0,0 +1,34 @@
+package com.test.arrayList;
+
+import java.util.ArrayList;
+
+public class First {
+	
+	public static void main(String args[]){
+		ArrayList<String> abc = new ArrayList<String>();
+		
+		abc.add("A");
+		abc.add("B");
+		abc.add("C");
+		abc.add("D");
+		
+		System.out.println("Here"+ abc);
+		
+		abc.add(0, "AA");
+		
+		System.out.println("Here2"+ abc);
+		
+		abc.remove(0);
+		
+		System.out.println("Here3"+ abc);
+		
+		abc.remove("C");
+		
+		System.out.println("Here4"+ abc);
+		
+		boolean a = abc.contains("A");
+		
+		System.out.println("Here5"+ a);
+	}
+	
+}
